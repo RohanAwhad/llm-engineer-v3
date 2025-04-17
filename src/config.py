2baad94b2b0b034682ca56595a3911397bb1125d
Rohan Awhad <rohanavad007@gmail.com>
2025-04-16 21:45:23 -0700

    fixed config; added traceloop to toml spec

--- src/config.py
+++ src/config.py
@@ -8,14 +8,6 @@ class Settings(BaseSettings):
     LANGFUSE_PUBLIC_KEY: str
     LANGFUSE_SECRET_KEY: str
 
-    # Configure settings to load from .env file
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        case_sensitive=True,
-    )
-
-
 # Create settings instance
 settings = Settings()
 
